27386dd82bb96b9d2c7080440f6a6c48e59fa1c0
Evan <evanpelletier3@cmail.carleton,ca>
2020-10-18 14:00:29 -0400

    fixing code

--- Lab5/CD.py
+++ Lab5/CD.py
@@ -1,6 +1,11 @@
 # SYSC3010 Lab 5 - CD.py
 #     The file to be jointly edited by Team Members C and D
 
+from C import *
+from D import *
+
 
 def function_CD():
-    print ("CD")
+    function_C()
+    function_D()
+    print("CD")
