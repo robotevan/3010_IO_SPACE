855e097f685a94dbb47717c85850508812ff64ca
Evan <evanpelletier3@cmail.carleton,ca>
2020-10-26 12:42:49 -0400

    Evan Pelletier type: feat
    Adding javascript for fade in effect. Used in section headers

---
+++ TeamProject/views/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+      $(".fadein").each(function() {
+        /* Check the location of each desired element */
+        var objectBottom = $(this).offset().top + $(this).outerHeight();
+        
+        /* If the element is completely within bounds of the window, fade it in */
+        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+            console.log("visible");
+          if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+        } else { //object goes out of view (scrolling up)
+          if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+        }
+      });
+    }).scroll(); //invoke scroll-handler on page-load
+});
