ba18d8228c0049490eca82c9e7818c5f72db51d6
Evan <evanpelletier3@cmail.carleton,ca>
2020-10-27 21:40:07 -0400

    Evan Pelletier type:Feat  adding animation for code on main page

--- TeamProject/views/main.js
+++ TeamProject/views/main.js
@@ -1,17 +1,34 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-      $(".fadein").each(function() {
-        /* Check the location of each desired element */
-        var objectBottom = $(this).offset().top + $(this).outerHeight();
-        
-        /* If the element is completely within bounds of the window, fade it in */
-        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-            console.log("visible");
-          if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-        } else { //object goes out of view (scrolling up)
-          if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-        }
-      });
-    }).scroll(); //invoke scroll-handler on page-load
-});
+// List of words to display on main page
+var words = ["code", "deploy", "dev", "🐒", "create", "test", "IOT", "🍌", "make", "WIFI", "EASY"]
+var currWord = 0;
+
+async function writeCode(){
+  while (true){
+    var word = words[currWord];
+    // Write each letter with 150ms delay
+    for(i = 0; i <= word.length; i++){
+      await new Promise(resolve => setTimeout(resolve, 150));
+      var txt = word.substring(0, i);
+      document.getElementById("codeWriter").innerHTML = txt;
+    }
+    await new Promise(resolve => setTimeout(resolve, 1500));
+    for(i = word.length; i >= 0; i--){
+      await new Promise(resolve => setTimeout(resolve, 150));
+      var txt = word.substring(0, i);
+      document.getElementById("codeWriter").innerHTML = txt;
+    }
+    // increment position in list of words
+    currWord++;
+    // reset to start of list if done
+    if(currWord >= words.length){
+      currWord = 0;
+    }
+  }
+}
+
+writeCode();
+
+
+$(document).scroll(function(){
+  var itemBot = $(this).posi              
+})
