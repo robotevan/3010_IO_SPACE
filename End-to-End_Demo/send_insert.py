c359baab6ef8a8a0213381bd0e4f18434bdac4ad
MG4CE <maged.othm@gmail.com>
2020-10-29 19:46:00 -0400

    Maged fix: added proper return for un/subscribe and publish

--- End-to-End_Demo/send_insert.py
+++ End-to-End_Demo/send_insert.py
@@ -42,11 +42,11 @@ def on_message(client, userdata, message):
     print("message retain flag =",message.retain)
     print("Is data inserted into db: ", insert_into_collection(collection, int(message.payload.decode("utf-8"))))
 
-def subscribe(mqtt_client: mqtt.Client, topic: str):
-    mqtt_client.subscribe(topic)
+def subscribe(mqtt_client: mqtt.Client, topic: str) -> tuple:
+    return mqtt_client.subscribe(topic)
 
-def unsubscribe(mqtt_client: mqtt.Client, topic: str):
-    mqtt_client.unsubscribe(topic)
+def unsubscribe(mqtt_client: mqtt.Client, topic: str) -> tuple:
+    return mqtt_client.unsubscribe(topic)
 
 def publish(mqtt_client: mqtt.Client, topic: str, message: str) -> bool:
     result = mqtt_client.publish(topic, message)
