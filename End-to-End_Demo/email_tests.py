0d106a40a46a4ff6477710dc73dc7abe97d5c9e9
pc <OUSAMAALCHAMI@CMAIL.CARLETON.CA>
2020-11-07 17:19:13 -0500

    this will send emails

---
+++ End-to-End_Demo/email_tests.py
@@ -0,0 +1,14 @@
+import smtplib, ssl
+context = ssl.create_default_context()
+server = smtplib.SMTP("smtp.gmail.com",587)
+server.ehlo()
+server.starttls(context=context)
+server.ehlo()
+server.login("testcasesg@gmail.com","123456789Ou*")
+#server.sendmail("testcasesg@gmail.com","ousamashami1999@gmail.com","hey, how are you")
+#server.quit()
+
+def sendemail(email_text,reciever):
+    server.sendmail("testcasesg@gmail.com",reciever,email_text)
+    print("sending an email with text : ",email_text,"to ",reciever)
+sendemail("this is a test email","evanpelletier3@cmail.carleton.ca")
