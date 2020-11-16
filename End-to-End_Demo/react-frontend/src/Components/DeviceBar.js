ad7bbcb9fbff9e3a1aaef83f78ebfd0712d1af39
Evan <evanpelletier3@cmail.carleton,ca>
2020-11-16 14:55:14 -0500

    Evan Pelletier Type: Feat, adding dynamic url device search, error catching

--- End-to-End_Demo/react-frontend/src/Components/DeviceBar.js
+++ End-to-End_Demo/react-frontend/src/Components/DeviceBar.js
@@ -4,13 +4,34 @@ import Device from './Device'
 
 
 class DeviceBar extends Component{
-    state = {devices: []}
+    state = {devices: [],
+            connected: false}
+
+
+    get_devices_style = () =>{
+        if (this.state.connected){
+            return("My Devices: ")
+        }else{
+            return("please enter a valid device name in the url api/<apikey>")
+        }
+
+    }
+
+    update = (data)=>{
+        if (data.devices === "NoneFound"){
+            this.setState({devices: []})
+            this.setState({connected: false})
+        }else{
+            this.setState({connected: true})
+            this.setState({devices: data.devices});
+        }
+    }
 
     // When component is loaded start polling database every 10 seconds, checking if sensors updated
     async componentDidMount(){
         setInterval(async () =>{
             fetch(window.location.pathname).then(res => res.json()).then(data => {
-                this.setState({devices: data.devices});
+                this.update(data)
               })
         }, 4000);
     }
@@ -18,7 +39,7 @@ class DeviceBar extends Component{
     render(){
         return(
             <div className="DeviceBar">
-                <div className="DeviceBarHeader">My Devices: {this.state.devices.length}</div>
+                <div className="DeviceBarHeader">{this.get_devices_style()}{this.state.devices.length}</div>
                 <div className="DeviceList">
                     {
                         this.state.devices.map((device)=> (
