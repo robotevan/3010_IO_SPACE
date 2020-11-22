6eea6dcc76293b41e2ba09bfd283bb8d84248c9a
MG4CE <maged.othm@gmail.com>
2020-11-22 02:24:46 -0500

    Maged feat: Added classes to client side API

--- TeamProject/ClientAPI/iospaceAPI.py
+++ TeamProject/ClientAPI/iospaceAPI.py
@@ -1,1 +1,22 @@
 #IOSpace Client Side API
+import paho
+
+node_ready = False
+MQTT_ADDRESS = ""
+
+class MQTT:
+
+    def __init__(self, client_name):
+        self.client_name = client_name
+
+
+class Node:
+    def __init__(self, name):
+        MQTT.__init__()
+
+    def setup(self):
+        print()
+
+class Device:
+    def __init__(self, name):
+        print(name)
