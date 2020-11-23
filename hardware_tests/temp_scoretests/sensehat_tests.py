6804fafa9ed4585530551ef459753e74b0cce940
pc <OUSAMAALCHAMI@CMAIL.CARLETON.CA>
2020-11-23 15:06:33 -0500

    these are some test cases for the sense hat

---
+++ hardware_tests/temp_scoretests/sensehat_tests.py
@@ -0,0 +1,28 @@
+from sense_emu import SenseHat
+import unittest
+
+sensehat=SenseHat()
+class TestSenseHat(unittest.TestCase):
+    def test_createsensehat(self):
+        self.assertIsNotNone(sensehat)
+
+    def test_readtemp(self):
+        self.assertIsNotNone(sensehat.temperature)
+
+    def test_humidity(self):
+        self.assertIsNotNone(sensehat.humidity)
+
+    def test_gyroscope(self):
+        self.assertIsNotNone(sensehat.get_gyroscope())
+
+    def test_accl(self):
+        self.assertIsNotNone(sensehat.get_accelerometer())
+
+    def test_comp(self):
+        self.assertIsNotNone(sensehat.compass)
+
+    def test_pres(self):
+        self.assertIsNotNone(sensehat.pressure)
+
+    if __name__ =='__main__':
+        unittest.main()
